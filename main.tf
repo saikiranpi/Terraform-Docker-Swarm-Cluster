7b56ef5803e4a8f1a6fcb9497f1bd33851ac05ad
Pinapathruni Saikiran <109568252+saikiranpi@users.noreply.github.com>
2024-07-07 02:56:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "swarm_sg" {
   name        = "swarm_sg"
   description = "Allow all inbound traffic and SSH"
 
-  ingress {
+ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -17,8 +17,24 @@ resource "aws_security_group" "swarm_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.1.1.0/32"]
   }
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["10.1.1.0/32"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.2.1.0/32"]
+  }
+
+
 
   egress {
     from_port   = 0
@@ -26,6 +42,11 @@ resource "aws_security_group" "swarm_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  # lifecycle {
+  #   ignore_changes = [
+  #     ingress,
+  #   ]
+  # }
 }
 
 resource "aws_instance" "swarm_master" {
